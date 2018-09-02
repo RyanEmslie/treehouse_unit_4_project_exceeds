24398cb2ddc1bf83b1e5b77a28e0ab6bc14b7577
RyanEmslie <ryan.emslie@gmail.com>
2018-09-02 15:59:13 -0400

    completed project

--- js/main.js
+++ js/main.js
@@ -4,22 +4,36 @@
 
 function startGame(){
     //This block of code sets up the board and resets variables
-    let count = 1;
+    let count = 0;
     $('#start').show();
     $('#board').hide();
     $('#finish').hide();
     $("ul.boxes li").removeClass('box-filled-1 box-filled-2');
     $('#finish').removeClass('screen-win-one screen-win-two screen-win-tie');
-    player1Moves = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
-    player2Moves = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+    player1Moves = [0,0,0,0,0,0,0,0,0];
+    computerMoves = [0,0,0,0,0,0,0,0,0];
     let player1Name;
+    $('#player1').removeClass('active');
+    $('#player2').removeClass('active');
 
 
-    //Click event tied to Start Game button
+
+
+
+
+
+//Click event tied to Start Game button
     $( "#start .button" ).click( function()  {
- //****************** EXTRA CREDIT ************************
- //I hard coded the input text boxes into the index.html file 
+//****************** EXTRA CREDIT ************************
+    //I hard coded the input text boxes into the index.html file 
+    count = 0;
+    //Resets the player1Moves and player2Moves objects
+    for (let i =0; i < 10; i++) {
+        player1Moves[i] = 0;
+        computerMoves[i] = 0;
+    }
     player1Name = $('#player1Input').val();
+     //Tests if Player 1 name was entered
        if ( $('#player1Input').val().length < 1 ) {
             alert('Please enter Player 1 Name');
             return;
@@ -31,99 +45,161 @@ function startGame(){
     });
 
 
-    //Hover event displaying possible move
+
+
+
+
+
+//Hover event displaying possible move
     $("ul.boxes li").hover(function(){
         $("ul.boxes li").removeClass('image-1 image-2');
         if ( $(this).hasClass('box-filled-1') || $(this).hasClass('box-filled-2') ) {
-        } else if ( $('#player1').hasClass('active') ) {
-            $(this).addClass('image-1');
         } else {
-            $(this).addClass('image-2');
-        }
+            $(this).addClass('image-1');
+        } 
     })
 
-    //Click event to put circle or cross
+
+
+
+
+
+
+//Click event to put circle or cross
     $("ul.boxes li").click(function(){
-        if ( count > 8 ) {
-            $('#player1').removeClass('active');
-            endGame();
-        }
+        $('#player1').addClass('active');
         
-        
-        //Fills in the board square if it is not already selected
+        //Player 1 fills in the board square if it is not already selected
         if ( $(this).hasClass('box-filled-1') || $(this).hasClass('box-filled-2') ) {
             return;
-        } else if ( $('#player1').hasClass('active') ) {
-            $(this).addClass('box-filled-1');
         } else {
-            $(this).addClass('box-filled-2');
+            $(this).addClass('box-filled-1');
         }
         
-        
-
-
-
-
-
-
-
-        //Assigns "score" to each player
-        if ( $('#player1').hasClass('active') ) {
-            let num = $(this).attr("id");
-            player1Moves[num] = player1Moves[num] + 1;
-        } else if ( $('#player2').hasClass('active') ) {
-            let num = $(this).attr("id");
-            player2Moves[num] = player2Moves[num] + 1;
-        };
-        
-        testGameOver();
+        //Record Player 1 Moves
+        let num = $(this).attr("id");
+        player1Moves[num-1] = 1;
+        //Calculated number of turns to help determine if a Tie is achieved
         count = count + 1;
-        changePlayer();  
+        if ( count > 8 ) {
+            $('#player1').removeClass('active')
+            $('#player2').removeClass('active')
+            endGame();
+            return;
+        }
+
+
+
+        //Tests Player 1 move to see if it is a winning move
+        testGameOver();
+        
+        //Logic for Computer Move
+        calcCompMov();    
+        
+        //For testing - logs the spaces occupied
+        console.log(`Player 1 moves ${player1Moves}`);
+        console.log(`Computer moves ${computerMoves}`);
+        $('#player1').addClass('active');
+        
+        
     });
 
 
+
+
+
+
+
+//Function that determines the computers move
+    const calcCompMov = () => {
+        $('#player1').removeClass('active')
+        $('#player2').addClass('active')
+            if (!$("ul.boxes li:nth-child("+ 5 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 5 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 5 +")").addClass('box-filled-2')
+                computerMoves[4] = 1;
+            } 
+            else if (!$("ul.boxes li:nth-child("+ 1 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 1 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 1 +")").addClass('box-filled-2')
+                computerMoves[0] = 1;
+            } 
+            else if (!$("ul.boxes li:nth-child("+ 3 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 3 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 3 +")").addClass('box-filled-2')
+                computerMoves[2] = 1;
+            } 
+            else if (!$("ul.boxes li:nth-child("+ 7 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 7 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 7 +")").addClass('box-filled-2')
+                computerMoves[6] = 1;
+            } 
+            else if (!$("ul.boxes li:nth-child("+ 9 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 9 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 9 +")").addClass('box-filled-2')
+                computerMoves[8] = 1;
+            }
+            else if (!$("ul.boxes li:nth-child("+ 2 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 2 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 2 +")").addClass('box-filled-2')
+                computerMoves[1] = 1;
+            } 
+            else if (!$("ul.boxes li:nth-child("+ 4 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 4 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 4 +")").addClass('box-filled-2')
+                computerMoves[3] = 1;
+            } 
+            else if (!$("ul.boxes li:nth-child("+ 6 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 6 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 6 +")").addClass('box-filled-2')
+                computerMoves[5] = 1;
+            } 
+            else if (!$("ul.boxes li:nth-child("+ 8 +")").hasClass('box-filled-1') && !$("ul.boxes li:nth-child("+ 8 +")").hasClass('box-filled-2')) {
+                $("ul.boxes li:nth-child("+ 8 +")").addClass('box-filled-2')
+                computerMoves[7] = 1;
+            } 
+            else {
+                //For Testing
+                console.log('All FALSE!!!!!!!');
+            } 
+            $('#player2').removeClass('active')
+            $('#player1').addClass('active')
+            count = count + 1;
+            testGameOver(); 
+    }
+
+
+
+//Game logic to determine winner of human vs. computer game
     const testGameOver = () => {
-        //Game logic to determine winner of human vs. human game
-        if ( ((player1Moves[1] + player1Moves[2] + player1Moves[3]) == 3)  ||
-                ((player1Moves[4] + player1Moves[5] + player1Moves[6]) == 3 ) ||
-                ((player1Moves[7] + player1Moves[8] + player1Moves[9]) == 3 ) ||
+        $('#player1').removeClass('active')
+        $('#player2').removeClass('active')
+
+        if ( ((player1Moves[0] + player1Moves[1] + player1Moves[2]) == 3)  ||
+                ((player1Moves[3] + player1Moves[4] + player1Moves[5]) == 3 ) ||
+                ((player1Moves[6] + player1Moves[7] + player1Moves[8]) == 3 ) ||
+                ((player1Moves[0] + player1Moves[3] + player1Moves[6]) == 3 ) ||
                 ((player1Moves[1] + player1Moves[4] + player1Moves[7]) == 3 ) ||
                 ((player1Moves[2] + player1Moves[5] + player1Moves[8]) == 3 ) ||
-                ((player1Moves[3] + player1Moves[6] + player1Moves[9]) == 3 ) ||
-                ((player1Moves[1] + player1Moves[5] + player1Moves[9]) == 3 ) ||
-                ((player1Moves[3] + player1Moves[5] + player1Moves[7]) == 3 ) )
+                ((player1Moves[0] + player1Moves[4] + player1Moves[8]) == 3 ) ||
+                ((player1Moves[2] + player1Moves[4] + player1Moves[6]) == 3 ) )
         {
+            $('#player1').addClass('active')
             endGame();
-        } else if ( ((player2Moves[1] + player2Moves[2] + player2Moves[3]) == 3)  ||
-                ((player2Moves[4] + player2Moves[5] + player2Moves[6]) == 3 ) ||
-                ((player2Moves[7] + player2Moves[8] + player2Moves[9]) == 3 ) ||
-                ((player2Moves[1] + player2Moves[4] + player2Moves[7]) == 3 ) ||
-                ((player2Moves[2] + player2Moves[5] + player2Moves[8]) == 3 ) ||
-                ((player2Moves[3] + player2Moves[6] + player2Moves[9]) == 3 ) ||
-                ((player2Moves[1] + player2Moves[5] + player2Moves[9]) == 3 ) ||
-                ((player2Moves[3] + player2Moves[5] + player2Moves[7]) == 3 ) )
+        } else if ( ((computerMoves[0] + computerMoves[1] + computerMoves[2]) == 3)  ||
+                ((computerMoves[3] + computerMoves[4] + computerMoves[5]) == 3 ) ||
+                ((computerMoves[6] + computerMoves[7] + computerMoves[8]) == 3 ) ||
+                ((computerMoves[0] + computerMoves[3] + computerMoves[6]) == 3 ) ||
+                ((computerMoves[1] + computerMoves[4] + computerMoves[7]) == 3 ) ||
+                ((computerMoves[2] + computerMoves[5] + computerMoves[8]) == 3 ) ||
+                ((computerMoves[0] + computerMoves[4] + computerMoves[8]) == 3 ) ||
+                ((computerMoves[2] + computerMoves[4] + computerMoves[6]) == 3 ) )
         {
+            $('#player2').addClass('active')
             endGame();
         }
     }
 
 
-    //Function changes the .active for player - called by click event
-    const changePlayer = () => {   
-        if ( $('#player1').hasClass('active') ) {
-            $('#player1').removeClass('active')
-            $('#player2').addClass('active');
-        } else {
-            $('#player1').addClass('active');
-            $('#player2').removeClass('active')
-        };
-    };
-
-    //Function closes board and displays 'Winner' - called in click event
+    
+//Function closes board and displays 'Winner' - called in click event
     const endGame = () => {
+        
         $('#board').hide();
         $('#finish').show().addClass('screen-win'); 
-        count = 0;
+        
         //If statement determining which winner/tie screen to display
         if ( $('#player1').hasClass('active') ) {
             $('#finish').addClass('screen-win-one');
@@ -134,20 +210,16 @@ function startGame(){
         } else {
             $('#finish').addClass('screen-win-tie')
             $('.message').text("It's a Tie!");
-        }
-        //Resets the player1Moves and player2Moves objects
-        for (let i =0; i < 10; i++) {
-            player1Moves[i] = 0;
-            player2Moves[i] = 0;
-        }
+        } 
+        console.log(`This is the count at end: ${count}`)
+        
     };
 
 
-    //When 'New Game' button is pressed the Start Game function is called
+//When 'New Game' button is pressed the Start Game function is called
     $("#finish .button").click(function(){
         startGame();
     });
-
 };
 
 
